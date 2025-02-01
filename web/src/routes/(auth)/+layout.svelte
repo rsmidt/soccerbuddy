2646a505acba6f60ae16d63932367c24179977a8
Ruben Smidt <ruben@rsmidt.dev>
2025-02-01 18:26:25 +0100

    fix(web): hide back button in auth routes

---
+++ web/src/routes/(auth)/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { configureScreen } from "$lib/components/screen/screen.svelte";
+  import type { LayoutProps } from "./$types";
+
+  configureScreen({ backButtonShown: false });
+
+  const { children }: LayoutProps = $props();
+</script>
+
+{@render children()}
